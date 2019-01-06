eed8e6460fceb7763a4bf3bea2e4aff5f07068f2
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2019-01-06 08:25:42 -0800

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var caesarVal = 1 + Math.floor(Math.random() * 200);
 console.log(caesarVal);
 
-if (caesarVal === 60) {
-    $(".img").html("<a class='img' href='https://i.imgur.com/AfPxIPj.jpg' target='blank'>photo</a>");
-} else if (caesarVal === 28) {
-    $(".img").html("<a class='img' href='https://i.imgur.com/njqoEDU.jpg' target='blank'>photo</a>");
-} else if (caesarVal === 36) {
-    $(".img").html("<a class='img' href='https://i.imgur.com/CE9Y10Z.jpg' target='blank'>photo</a>");
-} else if (caesarVal === 100) {
-    $(".img").html("<a class='img' href='https://i.imgur.com/JfelP3Z.jpg' target='blank'>photo</a>");
-}
+$("strong").click(function() {
+    if (caesarVal === 60) {
+        $(".img").html("<img class='max' src='https://i.imgur.com/AfPxIPj.jpg'>");
+    } else if (caesarVal === 28) {
+        $(".img").html("<img class='max' src='https://i.imgur.com/njqoEDU.jpg'>");
+    } else if (caesarVal === 36) {
+        $(".img").html("<img class='max'src='https://i.imgur.com/CE9Y10Z.jpg'>");
+    } else if (caesarVal === 100) {
+        $(".img").html("<img class='max' src='https://i.imgur.com/JfelP3Z.jpg'>");
+    }
+});
 
 $(document).ready(function() {
     if ((caesarVal === 60) || (caesarVal === 28) || (caesarVal === 36) || (caesarVal === 100)) {
